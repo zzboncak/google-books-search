70d2064825aa1b835e641d8af92c7dc9fbbf8a35
Zachary <zachary.zboncak@gmail.com>
2019-11-17 07:46:21 -0600

    made API call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchForm from './SearchForm/SearchForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Google Books Search</h1>
+        </header>
+        <SearchForm />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/SearchForm/SearchForm.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import './SearchForm.css'
+
+class SearchForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            book_search: "",
+            books: []
+        }
+    }
+    
+    userTyped(input) {
+        this.setState({
+            book_search: input,
+        })
+    }
+
+    buildBooks(obj) {
+        //do stuff
+        let books = obj.items.map(item => {
+            return {
+                title: item.volumeInfo.title,
+                authors: item.volumeInfo.authors,
+                //price: `$${item.saleInfo.retailPrice["amount"]}`,
+                description: item.volumeInfo.description,
+                image: item.volumeInfo.imageLinks["smallThumbnail"]
+            }
+        });
+        console.log(books);
+        return books;
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const bookToSearch = this.state.book_search.replace(/ /g, "+");
+        const baseUrl = "https://www.googleapis.com/books/v1/volumes?key=AIzaSyBTtn-V7onqctCynDtvzWrRjS_cKEzV5VE&q=";
+        let url = baseUrl + bookToSearch;
+        fetch(url)
+            .then(response => {
+                if(!response.ok){
+                    throw new Error(`Something went wrong:`);
+                } else {
+                    return response;
+                }
+            })
+            .then(response => response.json())
+            .then(data => this.buildBooks(data))
+            .catch(err => console.log(err.message));
+    }
+
+    render() {
+        return(
+            <form onSubmit={e => this.handleSubmit(e)}>
+                <label htmlFor="book">Search: </label>
+                <input 
+                    type="text" 
+                    value={this.state.book_search} 
+                    name="book" 
+                    onChange={e => this.userTyped(e.target.value)}
+                    placeholder="Lord of the Rings"
+                    />
+                <button type="submit">Search books</button>
+            </form>
+        )
+    }
+}
+
+export default SearchForm;
